14355059d17667f2f4a949ab80938ffe55c574b4
mkrzywanski <michak964@gmail.com>
2020-12-23 15:09:13 +0100

    change css

--- chat-client/src/chat/message-field/ChatMessageField.js
+++ chat-client/src/chat/message-field/ChatMessageField.js
@@ -45,7 +45,7 @@ class ChatMessageField extends React.Component {
                     <textarea className="form-control chat-text-field" value={this.state.value} onChange={this.handleTextChange}></textarea>
                     <button className="btn btn-light btn-block" onClick={this.send}>Send</button>
                 </div>
-                <div className="input-group">
+                <div className="row input-group">
                     <span className="input-group-text" id="basic-addon1">UserName</span>
                     <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={this.state.userName} onChange={this.handleUserNameChange} />
                 </div>
